5b4e61db32170946b7642f269e3227e94f72c987
Ashish haldar <105638023+webdev2190@users.noreply.github.com>
2022-10-14 19:17:45 +0530

    Updates

---
+++ precache-manifest.78597ef2b9cb0242d0694e44c0cf3dff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "301582ce5c8c903a5c74",
+    "url": "/ide-project/static/js/main.301582ce.chunk.js"
+  },
+  {
+    "revision": "5b38c921df95db731888",
+    "url": "/ide-project/static/js/1.5b38c921.chunk.js"
+  },
+  {
+    "revision": "be2cb37dc53934884fdd",
+    "url": "/ide-project/static/js/runtime~main.be2cb37d.js"
+  },
+  {
+    "revision": "7d7c8855edee4b0c713d984ae6d52c2a",
+    "url": "/ide-project/index.html"
+  }
+];
